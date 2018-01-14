f15c784c059427f5d8a157f67fe374200d7d9aa9
Bernie Chiu <bernie_chiu@hotmail.com>
2018-01-14 10:16:33 +0800

    Set enum constant for original access

--- lib/mongoid/enum_mapper/define_enum.rb
+++ lib/mongoid/enum_mapper/define_enum.rb
@@ -9,18 +9,25 @@ module Mongoid
     module ClassMethods
       def define_enum(enum_field, enum_mapping = {})
         field :"#{enum_field}", type: Integer, default: 0
+        set_enum_mapping_constant(enum_field, enum_mapping)
 
         enum_value = :"@#{enum_field}"
 
         define_method(enum_field) do
-          instance_variable_get(enum_value) || instance_variable_set(enum_value, enum_mapping.find { |_, value| value == self[enum_field] }.try(:[], 0))
+          instance_variable_get(enum_value) ||
+          instance_variable_set(enum_value, enum_mapping.find { |_, value| value == self[enum_field] }.try(:[], 0))
         end
 
         define_method(:"#{enum_field}=") do |key|
           return self[enum_field] unless enum_mapping[key]
-          instance_variable_set(enum_value, nil) # clean mapping cache
+          instance_variable_set(enum_value, nil)
           self[enum_field] = enum_mapping[key]
         end
+      end
+
+      def set_enum_mapping_constant(name, value)
+        const_name = name.to_s.upcase
+        const_set(const_name, value)
       end
     end
   end
