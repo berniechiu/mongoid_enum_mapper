aa75af6dc406cfeaf5b3a44cd59b10c8b39bdef7
Bernie Chiu <bernie_chiu@hotmail.com>
2018-01-14 11:30:28 +0800

    Allow correct update for ORM methods

--- lib/mongoid/enum_mapper/define_enum.rb
+++ lib/mongoid/enum_mapper/define_enum.rb
@@ -6,10 +6,33 @@ module Mongoid
       base.extend(ClassMethods)
     end
 
+    def reload
+      super
+      reset_enum_mapping_cache
+      self
+    end
+
+    def reset_enum_mapping_cache(enum_field: nil, key: nil)
+      if enum_field
+        # Reset certain enum field
+        enum_mapper = self.class.const_get(enum_field.to_s.upcase)
+        key ||= enum_mapper.key(self[:"#{enum_field}"])  # If key not found, grep from current db value
+        return self[enum_field] unless enum_mapper[key]
+        instance_variable_set(:"@#{enum_field}", nil)
+        self[enum_field] = enum_mapper[key]
+      else
+        # Reset all enum field
+        self.class.instance_variable_get(:'@current_enums').each do |field|
+          reset_enum_mapping_cache(enum_field: field) 
+        end
+      end
+    end
+
     module ClassMethods
       def define_enum(enum_field, enum_mapping = {})
         field :"#{enum_field}", type: Integer, default: 0
         set_enum_mapping_constant(enum_field, enum_mapping)
+        set_current_enum_fields(enum_field)
 
         enum_value = :"@#{enum_field}"
 
@@ -19,15 +42,18 @@ module Mongoid
         end
 
         define_method(:"#{enum_field}=") do |key|
-          return self[enum_field] unless enum_mapping[key]
-          instance_variable_set(enum_value, nil)
-          self[enum_field] = enum_mapping[key]
+          reset_enum_mapping_cache(enum_field: enum_field, key: key)
         end
       end
 
       def set_enum_mapping_constant(name, value)
         const_name = name.to_s.upcase
-        const_set(const_name, value)
+        const_set(const_name, value.freeze)
+      end
+
+      def set_current_enum_fields(name)
+        @current_enums ||= []
+        @current_enums << name.to_s
       end
     end
   end
--- test/mongoid/define_enum_test.rb
+++ test/mongoid/define_enum_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class Mongoid::EnumMapper::DefineEnumTest < Minitest::Test
+  Minitest.after_run do
+    Mongoid.purge!
+  end
+
   def test_that_it_can_map_the_enum
     order_delivery = OrderDelivery.new
     assert_kind_of Integer, order_delivery[:status]
@@ -13,5 +17,15 @@ class Mongoid::EnumMapper::DefineEnumTest < Minitest::Test
     order_delivery.status = :shipping
     assert_equal 1, order_delivery[:status]
     assert_equal :shipping, order_delivery.status
+  end
+
+  def test_that_it_can_update_with_mongoid_orm_methods
+    order_delivery = OrderDelivery.new
+    order_delivery.save
+    assert_equal :pending, order_delivery.status
+    order_delivery.update(status: :shipped)
+    assert_equal :shipped, order_delivery.status
+    OrderDelivery.update_all(status: OrderDelivery::STATUS[:shipping])
+    assert_equal :shipping, order_delivery.reload.status
   end
 end
