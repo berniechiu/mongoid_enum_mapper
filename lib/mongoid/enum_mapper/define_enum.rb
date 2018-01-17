bb30cb876ee4ca1d9662d41c4f1c9fdd4b0301cb
Bernie Chiu <bernie_chiu@hotmail.com>
2018-01-17 22:19:07 +0800

    Make use of ActiveSupport::Concern inheritence

--- lib/mongoid/enum_mapper/define_enum.rb
+++ lib/mongoid/enum_mapper/define_enum.rb
@@ -3,33 +3,31 @@ module Mongoid
   module EnumMapper
     extend ActiveSupport::Concern
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
+    included do
+      def reload
+        super
+        reset_enum_mapping_cache
+        self
+      end
 
-    def reload
-      super
-      reset_enum_mapping_cache
-      self
-    end
-
-    def reset_enum_mapping_cache(enum_field: nil, key: nil)
-      if enum_field
-        # Reset certain enum field
-        enum_mapper = self.class.const_get(enum_field.to_s.upcase)
-        key ||= enum_mapper.key(self[:"#{enum_field}"])  # If key not found, grep from current db value
-        return self[enum_field] unless enum_mapper[key]
-        instance_variable_set(:"@#{enum_field}", nil) if self.status != enum_mapper[key]
-        self[enum_field] = enum_mapper[key]
-      else
-        # Reset all enum field
-        self.class.instance_variable_get(:'@current_enums').each do |field|
-          reset_enum_mapping_cache(enum_field: field)
+      def reset_enum_mapping_cache(enum_field: nil, key: nil)
+        if enum_field
+          # Reset certain enum field
+          enum_mapper = self.class.const_get(enum_field.to_s.upcase)
+          key ||= enum_mapper.key(self[:"#{enum_field}"])  # If key not found, grep from current db value
+          return self[enum_field] unless enum_mapper[key]
+          instance_variable_set(:"@#{enum_field}", nil) if self.status != enum_mapper[key]
+          self[enum_field] = enum_mapper[key]
+        else
+          # Reset all enum field
+          self.class.instance_variable_get(:'@current_enums').each do |field|
+            reset_enum_mapping_cache(enum_field: field)
+          end
         end
       end
     end
 
-    module ClassMethods
+    class_methods do
       def define_enum(enum_field, enum_mapping = {})
         field :"#{enum_field}", type: Integer, default: 0
         set_enum_mapping_constant(enum_field, enum_mapping)
